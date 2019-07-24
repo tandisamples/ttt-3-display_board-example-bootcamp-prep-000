f22817274400f57ef9ca41079195726f63fb55b7
Tandi Samples <tandisamples@gmail.com>
2019-07-24 02:40:02 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,4 @@
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
+  // ♥ irb
+001:0 > def display_board
+002:1 >     puts "A Tic Tac Toe Board"
+003:1 >   end
